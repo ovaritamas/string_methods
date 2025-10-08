b0296c57e94431ce8c6298a67b431a0fd06f552f
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-10-08 09:30:04 +0200

    4.feladat

--- 04_szocsere.py
+++ 04_szocsere.py
@@ -2,6 +2,12 @@
 4️⃣ Szó cseréje másikra – ételrendelés módosítása
 Feladat: A rendelésben az „alma” helyett cseréld „körtére”, ha a boltban nincs alma.
 """
+bolt = ["körte", ""]
 rendeles = input("Add meg a rendelést amával: ")
-modositas = rendeles.replace("alma, körte")
-print(modositas)
+
+print(rendeles)
+if "alma" in bolt:
+    print("Van alma a boltban!")
+else:
+    modositas = rendeles.replace("alma", "körte")
+    print(modositas)
